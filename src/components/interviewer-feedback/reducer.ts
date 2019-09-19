1292a3fabc95761e44840a5bb26b64da1b19dbf2
diego-torres <dtorresf@redhat.com>
2019-09-19 13:53:11 -0500

    reducer for additional interviewer action

--- src/components/interviewer-feedback/reducer.ts
+++ src/components/interviewer-feedback/reducer.ts
@@ -1,12 +1,14 @@
 import {
     InterviewerFeedbackActionTypes,
     IInterviewerFeedbackModalState,
+    ADDITIONAL_INTERVIEWER_TOGGLE,
     INTERVIEWER_FEEDBACK_TOGGLE,
     INTERVIEWER_FEEDBACK_SCORE_CHANGE,
     INTERVIEWER_FEEDBACK_COMMENT_CHANGE
 } from './types';
 
 const initialState: IInterviewerFeedbackModalState = {
+    additionalVisible: false,
     candidateScore: '',
     comment: '',
     error_message: '',
@@ -23,6 +25,7 @@ export function interviewerFeedbackModalReducer(
         case INTERVIEWER_FEEDBACK_TOGGLE: { return { ...state, modalVisible: !state.modalVisible } }
         case INTERVIEWER_FEEDBACK_SCORE_CHANGE: { return { ...state, candidateScore: action.value } }
         case INTERVIEWER_FEEDBACK_COMMENT_CHANGE: { return { ...state, comment: action.value } }
+        case ADDITIONAL_INTERVIEWER_TOGGLE: { return { ...state, additionalVisible: !state.additionalVisible } }
         default: return state;
     }
 }
