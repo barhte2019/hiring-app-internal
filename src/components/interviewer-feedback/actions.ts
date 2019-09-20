2e61c7676f2907d50c7c7dc1d3a04d648f0be587
diego-torres <dtorresf@redhat.com>
2019-09-19 20:32:33 -0500

    dynamic change fields actions

--- src/components/interviewer-feedback/actions.ts
+++ src/components/interviewer-feedback/actions.ts
@@ -2,7 +2,9 @@ import {
     ADDITIONAL_INTERVIEWER_TOGGLE,
     INTERVIEWER_FEEDBACK_TOGGLE,
     INTERVIEWER_FEEDBACK_SCORE_CHANGE,
-    INTERVIEWER_FEEDBACK_COMMENT_CHANGE
+    INTERVIEWER_FEEDBACK_COMMENT_CHANGE,
+    DYNAMIC_TASK_MESSAGE_CHANGE,
+    DYNAMIC_TASK_RECIPIENT_CHANGE
 } from './types';
 
 export function interviewerFeedbackToggle() {
@@ -19,4 +21,12 @@ export function interviewerFeedbackCommentChange(value: string) {
 
 export function toggleAdditionalInterviewer() {
     return { type: ADDITIONAL_INTERVIEWER_TOGGLE }
+}
+
+export function dynamicTaskMessageChange(value: string) {
+    return { type: DYNAMIC_TASK_MESSAGE_CHANGE, value }
+}
+
+export function dynamicTaskRecipientChange(value: string) {
+    return { type: DYNAMIC_TASK_RECIPIENT_CHANGE, value }
 }
