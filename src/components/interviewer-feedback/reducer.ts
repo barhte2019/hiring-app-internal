00c43ea66a102fbdea2fa615041ef44a8758ad0a
diego-torres <dtorresf@redhat.com>
2019-09-19 20:43:01 -0500

    reducer changes to state based on dynamic task fields

--- src/components/interviewer-feedback/reducer.ts
+++ src/components/interviewer-feedback/reducer.ts
@@ -4,13 +4,17 @@ import {
     ADDITIONAL_INTERVIEWER_TOGGLE,
     INTERVIEWER_FEEDBACK_TOGGLE,
     INTERVIEWER_FEEDBACK_SCORE_CHANGE,
-    INTERVIEWER_FEEDBACK_COMMENT_CHANGE
+    INTERVIEWER_FEEDBACK_COMMENT_CHANGE,
+    DYNAMIC_TASK_MESSAGE_CHANGE,
+    DYNAMIC_TASK_RECIPIENT_CHANGE
 } from './types';
 
 const initialState: IInterviewerFeedbackModalState = {
     additionalVisible: false,
     candidateScore: '',
     comment: '',
+    dt_message: '',
+    dt_recipient: '',
     error_message: '',
     loading: false,
     modalVisible: false,
@@ -26,6 +30,8 @@ export function interviewerFeedbackModalReducer(
         case INTERVIEWER_FEEDBACK_SCORE_CHANGE: { return { ...state, candidateScore: action.value } }
         case INTERVIEWER_FEEDBACK_COMMENT_CHANGE: { return { ...state, comment: action.value } }
         case ADDITIONAL_INTERVIEWER_TOGGLE: { return { ...state, additionalVisible: !state.additionalVisible } }
+        case DYNAMIC_TASK_MESSAGE_CHANGE: { return { ...state, dt_message: action.value } }
+        case DYNAMIC_TASK_RECIPIENT_CHANGE: { return { ...state, dt_recipient: action.value } }
         default: return state;
     }
 }
